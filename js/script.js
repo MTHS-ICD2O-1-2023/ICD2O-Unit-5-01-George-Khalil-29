a1ad73d291c911e6d440a5b86437af3a96125de8
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-04-23 00:28:48 +0000

    save

--- js/script.js
+++ js/script.js
@@ -3,3 +3,27 @@
 // Created by: George Khalil
 // Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+/**
+ * This function checks if the user has selected the random number form above
+ */
+
+function checkNumber(){
+  // input
+  const usernumber = parseInt(document.getElementById("user-number").value)
+
+  // process
+  if (userNumber == randomNumber){
+    // output
+    document.getElementById("result").innerHTML = 
+     "You have guessed the correst number!"
+  }
+
+  if (userNumber != randomNumnber) {
+    // output
+    document.getElementById("result").innerHTML =
+      "You have guessed the wrong number!u "
+  }
+}
